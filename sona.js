69dbe29f3171608fcbc418eea796f3f64c73aa02
Rob Mastronicola <arejaycola@gmail.com>
2021-04-19 21:13:40 -0400

    Added a why wait section for ordering now

--- sona.js
+++ sona.js
@@ -5,29 +5,35 @@ $(function () {
 	$('.order-modal__header__close').click(function () {
 		$('.order-modal').addClass('order-modal--hidden');
 	});
-
-	$('#event').hide();
-	$('#cakeNmore').hide();
-	$('#closeForm').hide();
-	$('#order').change(function () {
-		orderType = $(this).val();
-		if (orderType === 'Event') {
-			$('#cakeNmore').hide();
-			$('#event').show();
-			$('#closeForm').show();
-		}
-		if (orderType === 'Cakes and Dessert') {
-			$('#event').hide();
-			$('#cakeNmore').show();
-			$('#closeForm').show();
-		}
+	$('.creations__quote-button').click(function () {
+		$('.order-modal').toggleClass('order-modal--hidden');
+	});
+	$('.order-modal__header__close').click(function () {
+		$('.order-modal').addClass('order-modal--hidden');
 	});
 
-	$('#closeForm').click(function () {
-		$('#event').hide();
-		$('#cakeNmore').hide();
-		$(this).hide();
-	});
+	// $('#event').hide();
+	// $('#cakeNmore').hide();
+	// $('#closeForm').hide();
+	// $('#order').change(function () {
+	// 	orderType = $(this).val();
+	// 	if (orderType === 'Event') {
+	// 		$('#cakeNmore').hide();
+	// 		$('#event').show();
+	// 		$('#closeForm').show();
+	// 	}
+	// 	if (orderType === 'Cakes and Dessert') {
+	// 		$('#event').hide();
+	// 		$('#cakeNmore').show();
+	// 		$('#closeForm').show();
+	// 	}
+	// });
+
+	// $('#closeForm').click(function () {
+	// 	$('#event').hide();
+	// 	$('#cakeNmore').hide();
+	// 	$(this).hide();
+	// });
 
 	// $('.overlay').click(function(){
 	//     $(this).toggleClass('overlayOnOff')
