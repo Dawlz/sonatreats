3b207f20668afe7d03385838f3ee6329a41166e9
Rob Mastronicola <arejaycola@gmail.com>
2021-04-21 22:47:44 -0400

    Updated modal

--- sona.js
+++ sona.js
@@ -1,15 +1,42 @@
 $(function () {
+	let lineCount = 1;
+	let nextId = 1;
+
 	$('.hero__cta-button').click(function () {
 		$('.order-modal').toggleClass('order-modal--hidden');
+		$('body').toggleClass('modal-open');
 	});
 	$('.order-modal__header__close').click(function () {
 		$('.order-modal').addClass('order-modal--hidden');
+		$('body').toggleClass('modal-open');
 	});
 	$('.creations__quote-button').click(function () {
 		$('.order-modal').toggleClass('order-modal--hidden');
+		$('body').toggleClass('modal-open');
 	});
 	$('.order-modal__header__close').click(function () {
 		$('.order-modal').addClass('order-modal--hidden');
+		$('body').toggleClass('modal-open');
+	});
+
+	$('.add-line').click(function (e) {
+		e.preventDefault();
+
+		$('.line-item').first().clone().prop('id', `line-${nextId}`).appendTo('.order-modal__item-selection');
+		$('.order-modal__item-selection').append($('.add-line'));
+		lineCount++;
+		nextId++;
+	});
+
+	$(document).on('click', '.line-item__remove', function (e) {
+		e.preventDefault();
+
+		if (lineCount > 1) {
+			const lineId = $(this).parent().prop('id');
+			$(`.line-item#${lineId}`).remove();
+
+			lineCount--;
+		}
 	});
 
 	// $('#event').hide();
